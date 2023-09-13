57ccd6ea8a7a71814d11963560eae457a5366c30
akrauttam <dipu88124@gmail.com>
2023-09-13 13:43:18 +0530

    fixed by uttam

--- problem1.js
+++ problem1.js
@@ -1,14 +1,18 @@
-let num = 11 ;
-let cnt = 0 ;
-let bag = "" ;
-for(let i = 2 ; i <= num**(0.5) ; i++){
-    if(num % i == 0){
-        cnt ++ ; 
+let num = 11;
+let cnt = 0;
+let bag = "";
+for (let i = 2; i <= num ** (0.5); i++) {
+    if (num % i == 0) {
+        cnt++;
     }
 }
-if(cnt == 0){
-    bag += "prime" ;
-}else{
-    bag += "Composite" ;
+if (cnt == 0) {
+    bag += "prime";
+} else {
+    bag += "Composite";
 }
 console.log(bag);
+
+
+
+console.log("change");
